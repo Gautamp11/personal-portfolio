e5d73ccdd6656791851d099a4658142ec04c818d
gautamp11 <gautam1564p1@gmail.com>
2026-01-11 14:32:52 +0530

    updated live link

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,79 +4,91 @@ const Projects = () => {
       title: "Time Hop",
       image: "project-timehop.png",
       skills: ["NextJS", "tailwind", "Supabase"],
-      link: "https://github.com/Gautamp11/timehop",
+      gitLink: "https://github.com/Gautamp11/timehop",
+      liveLink: "https://timehop.vercel.app/",
     },
     {
       title: "Smart Finance Observer",
       image: "project-smart-finance.png",
       skills: ["NextJS", "tailwind", "Supabase"],
-      link: "https://github.com/Gautamp11/smartFinace",
+      gitLink: "https://github.com/Gautamp11/smart-finance",
+      liveLink: "https://smart-finance-theta.vercel.app/",
     },
     {
       title: "Skill Share Board",
       image: "project-skillShareBoard.png",
       skills: ["React", "tailwind", "Supabase"],
-      link: "https://github.com/Gautamp11/skillShareBoard",
+      gitLink: "https://github.com/Gautamp11/skillShareBoard",
+      liveLink: "https://skill-share-board.vercel.app/",
     },
     {
       title: "The Wild Oasis Hotel Management App",
       image: "project-the-wild-oasis.png",
       skills: ["React", "React Query", "Supabase"],
       link: "https://github.com/Gautamp11/the-wild-oasis",
+      liveLink: "https://the-wild-oasis-gautamp1.vercel.app/",
     },
     {
       title: "The Wild Oasis Website",
       image: "project-the-wild-oasis-website.png",
       skills: ["NextJS", "Tailwind", "Supabase"],
-      link: "https://github.com/Gautamp11/the-wild-oasis-website",
+      gitLink: "https://github.com/Gautamp11/the-wild-oasis-website",
+      liveLink: "https://the-wild-oasis-website-gautamp1.vercel.app/",
     },
     {
       title: "Today I Learned",
       image: "project-today-i-learned.png",
       skills: ["React", "Supabase"],
-      link: "https://github.com/Gautamp11/today-i-learned",
+      gitLink: "https://github.com/Gautamp11/today-i-learned",
+      liveLink: "https://today-i-learned-sooty.vercel.app/",
     },
     {
       title: "Chit Chat",
       image: "project-chit-chat.png",
       skills: ["React", "Supabase", "Tailwind"],
-      link: "https://github.com/Gautamp11/chit-chat",
+      gitLink: "https://github.com/Gautamp11/chit-chat",
+      liveLink: "",
     },
     {
       title: "Habit Tracker",
       image: "project-habit-tracker.png",
       skills: ["React", "tailwind", "Redux"],
       link: "https://github.com/Gautamp11/habit-tracker",
+      liveLink: "https://habit-tracker-gautamp1.vercel.app/",
     },
     {
       title: "React Quiz App",
       image: "project-react-quiz.png",
       skills: ["React", "CSS", "JSX"],
-      link: "https://github.com/Gautamp11/rock-paper-scissors-game",
+      gitLink: "https://github.com/Gautamp11/rock-paper-scissors-game",
+      liveLink: "https://react-quiz-gautamp1.vercel.app/",
     },
     {
       title: "Travel List",
       image: "project-react-travel-list.png",
       skills: ["React", "CSS", "JSX"],
-      link: "https://github.com/Gautamp11/rock-paper-scissors-game",
+      gitLink: "https://github.com/Gautamp11/rock-paper-scissors-game",
+      liveLink: "",
     },
     {
       title: "Fast React Pizza App",
       image: "project-react-pizza-app.jpg",
       skills: ["React", "Tailwind", "Redux"],
-      link: "https://github.com/Gautamp11/fast-react-pizza",
-    },
-    {
-      title: "50 Mini Projects",
-      image: "project-50-mini.jpeg",
-      skills: ["HTML", "CSS", "JS"],
-      link: "https://github.com/Gautamp11/50-projects-in-50-days",
+      gitLink: "https://github.com/Gautamp11/fast-react-pizza",
+      liveLink: "",
     },
     {
       title: "Weather App",
       image: "project-weather-app.jpeg",
       skills: ["HTML", "CSS", "JS"],
-      link: "https://github.com/Gautamp11/weather-app",
+      gitLink: "https://github.com/Gautamp11/weather-app",
+      liveLink: "https://weather-app-gautamp1.netlify.app/",
+    },
+    {
+      title: "50 Mini Projects",
+      image: "project-50-mini.jpeg",
+      skills: ["HTML", "CSS", "JS"],
+      gitLink: "https://github.com/Gautamp11/50-projects-in-50-days",
     },
   ];
 
@@ -103,9 +115,26 @@ const Projects = () => {
                   ))}
                 </div>
               </div>
-              <a href={project.link} target="_blank" rel="noopener noreferrer">
-                <i className="ri-arrow-right-up-line"></i>
-              </a>
+              <div>
+                <a
+                  href={project.gitLink}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  title="Github Link"
+                >
+                  <i className="ri-code-line"></i>
+                </a>
+                {project.liveLink && (
+                  <a
+                    href={project.liveLink}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    title="Live Link"
+                  >
+                    <i className="ri-global-line"></i>
+                  </a>
+                )}
+              </div>
             </div>
           </div>
         ))}
